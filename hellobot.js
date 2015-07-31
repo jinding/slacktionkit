5f75503763d9001bda9356dfd8779109d222fe8f
Jin Ding <jinding25@gmail.com>
2015-07-31 15:55:44 -0700

    Revert "test change of output"
    This reverts commit c60e2855874762c700eca508f44244972e094b2e.

--- hellobot.js
+++ hellobot.js
@@ -1,7 +1,7 @@
 module.exports = function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
-    'Hello, ' + userName + '!'
+    text : 'Hello, ' + userName + '!'
   }
 
   // avoid infinite loop
