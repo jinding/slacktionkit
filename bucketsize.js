a9f8f1d71b559e2db70aef322f6839503638d8e1
Jin Ding <jinding25@gmail.com>
2015-07-31 16:36:17 -0700

    Update response to simple string

--- bucketsize.js
+++ bucketsize.js
@@ -2,17 +2,11 @@ module.exports = function (req, res, next) {
   var userName = req.body.user_name;
   var tag = req.body.text;
 
-  if (tag) {
-    var botPayload = {
-      text : 'There are ### current members tagged ' + tag + '.'
-    }
-  } else {
-    var botPayload = {
-      text : "You didn't enter a tag."
-    }
-
-  }
-
+  if (tag) 
+    var botPayload = 'There are ### current members tagged ' + tag + '.';
+  else 
+    var botPayload = "You didn't enter a tag.";
+  
   return res.status(200).json(botPayload);
   
   //  return res.status(200).end(); -- for errors
