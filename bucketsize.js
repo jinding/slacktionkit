ab4a3c9657a8efda472d0b3e8fda55020d5a5faf
Jin Ding <jinding25@gmail.com>
2015-07-31 22:00:29 -0700

    update to use AK report with cache set to 3600 (1 hr)

--- bucketsize.js
+++ bucketsize.js
@@ -6,11 +6,12 @@ module.exports = function (req, res, next) {
   console.log(bucket);
 
   request({
-      url: 'https://act.credoaction.com/rest/v1/report/run/sql/',
+      url: 'https://act.credoaction.com/rest/v1/report/run/list_issue_group_size_specific/',
       auth: { user: 'meteor', pass: 'dingbergalis' },
       qs: {
-            'query': "SELECT COUNT(*) FROM core_user_page_tags upt JOIN core_tag t ON t.id = upt.tag_id JOIN core_subscription s ON s.user_id = upt.user_id AND s.list_id = 1 WHERE t.name = {{ tag }}",
-            'tag': bucket
+//            'query': "SELECT COUNT(*) FROM core_user_page_tags upt JOIN core_tag t ON t.id = upt.tag_id JOIN core_subscription s ON s.user_id = upt.user_id AND s.list_id = 1 WHERE t.name = {{ tag }}",
+            'tag': bucket,
+            'cache_duration': '3600' 
           },
       method: 'POST', //Specify the method
       headers: {
@@ -22,9 +23,9 @@ module.exports = function (req, res, next) {
           var bot = 'There are currently '+body.replace('[[','').replace(']]','')+' members tagged '+bucket;
           return res.status(200).send(bot);
       } else if (response && response.statusCode) {
-          console.log('error: ' + error + ' ' + body);
-          return res.status(response.statusCode).send(body);
-      } else return res.status(400).send('undefined error');
+          console.log('error: ' + response.statusCode + ' ' + body);
+          return res.status(200).send(body);
+      } else return res.status(400).send(body);
   });
 
 }
