59dabad4cc3ab83b9795279ba2b19460d03d3fb6
Jin Ding <jinding25@gmail.com>
2015-07-31 22:03:02 -0700

    return visible error to user if no tag is specified

--- bucketsize.js
+++ bucketsize.js
@@ -5,27 +5,30 @@ module.exports = function (req, res, next) {
   var bucket = req.body.text;
   console.log(bucket);
 
-  request({
-      url: 'https://act.credoaction.com/rest/v1/report/run/list_issue_group_size_specific/',
-      auth: { user: 'meteor', pass: 'dingbergalis' },
-      qs: {
-//            'query': "SELECT COUNT(*) FROM core_user_page_tags upt JOIN core_tag t ON t.id = upt.tag_id JOIN core_subscription s ON s.user_id = upt.user_id AND s.list_id = 1 WHERE t.name = {{ tag }}",
-            'tag': bucket,
-            'cache_duration': '3600' 
-          },
-      method: 'POST', //Specify the method
-      headers: {
-        'Content-Type': 'application/json'
-      }
-  }, function(error, response, body){
-      if (response && response.statusCode && response.statusCode == 200) {
-          console.log('no errors ' + response.statusCode + ' ' + body);
-          var bot = 'There are currently '+body.replace('[[','').replace(']]','')+' members tagged '+bucket;
-          return res.status(200).send(bot);
-      } else if (response && response.statusCode) {
-          console.log('error: ' + response.statusCode + ' ' + body);
-          return res.status(200).send(body);
-      } else return res.status(400).send(body);
-  });
-
+  if (bucket) {
+    request({
+        url: 'https://act.credoaction.com/rest/v1/report/run/list_issue_group_size_specific/',
+        auth: { user: 'meteor', pass: 'dingbergalis' },
+        qs: {
+  //            'query': "SELECT COUNT(*) FROM core_user_page_tags upt JOIN core_tag t ON t.id = upt.tag_id JOIN core_subscription s ON s.user_id = upt.user_id AND s.list_id = 1 WHERE t.name = {{ tag }}",
+              'tag': bucket,
+              'cache_duration': '3600' 
+            },
+        method: 'POST', //Specify the method
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    }, function(error, response, body){
+        if (response && response.statusCode && response.statusCode == 200) {
+            console.log('no errors ' + response.statusCode + ' ' + body);
+            var bot = 'There are currently '+body.replace('[[','').replace(']]','')+' members tagged '+bucket;
+            return res.status(200).send(bot);
+        } else if (response && response.statusCode) {
+            console.log('error: ' + response.statusCode + ' ' + body);
+            return res.status(200).send(body);
+        } else return res.status(400).send(body);
+    });
+  } else {
+    return res.status(200).send('Need to specify a tag');
+  }
 }
