3e859b09da88e4b997fdf550156283e1077f3d8a
Jin Ding <jinding25@gmail.com>
2015-07-31 21:33:53 -0700

    implement ActionKit call
    use request module for easier HTTP handling

--- bucketsize.js
+++ bucketsize.js
@@ -1,14 +1,29 @@
 module.exports = function (req, res, next) {
-  var userName = req.body.user_name;
-  var tag = req.body.text;
+  var querystring = require('querystring');
+  var request = require('request');
 
-  if (tag) 
-    var botPayload = 'There are ### current members tagged ' + tag + '.';
-  else 
-    var botPayload = "You didn't enter a tag.";
-  
-  return res.status(200).json(botPayload);
-  
-  //  return res.status(200).end(); -- for errors
-  
+  var bucket = req.body.text;
+  console.log(bucket);
+
+  request({
+      url: 'https://act.credoaction.com/rest/v1/report/run/sql/',
+      auth: { user: 'meteor', pass: 'dingbergalis' },
+      qs: {
+            'query': "SELECT COUNT(*) FROM core_user_page_tags upt JOIN core_tag t ON t.id = upt.tag_id JOIN core_subscription s ON s.user_id = upt.user_id AND s.list_id = 1 WHERE t.name = {{ tag }}",
+            'tag': bucket
+          },
+      method: 'POST', //Specify the method
+      headers: {
+        'Content-Type': 'application/json'
+      }
+  }, function(error, response, body){
+      if (response && response.statusCode && response.statusCode == 200) {
+          console.log('no errors ' + response.statusCode + ' ' + body);
+          return(body);
+      } else {
+          console.log('error: ' + error + ' ' + body);
+          return('error: ' + error + ' ' + body);
+      }
+  });
+
 }
